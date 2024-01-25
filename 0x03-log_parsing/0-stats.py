32595634028b8905d36b83ea5a061e26f0c1b271
Fadyy22 <fadyalaa441@gmail.com>
2024-01-26 01:06:25 +0200

    parse the log from stdin

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -12,34 +12,24 @@ if __name__ == "__main__":
         "404": 0,
         "405": 0,
     }
-    try:
-        while True:
-            counter += 1
-            log = input().split()
-            file_size = int(log[-1])
-            status_code = log[-2]
-            try:
-                status_codes[status_code] += 1
-            except Exception:
-                pass
+    while True:
+        counter += 1
+        log = input().split()
+        file_size = int(log[-1])
+        status_code = log[-2]
+        try:
+            status_codes[status_code] += 1
+        except Exception:
+            pass
 
-            total_size += file_size
+        total_size += file_size
 
-            if counter % 10 == 0:
-                print(f"Files size: {total_size}")
+        if counter % 10 == 0:
+            print(f"Files size: {total_size}")
 
-                for key, value in status_codes.items():
-                    if value != 0:
-                        print(f"{key}: {value}")
-                        status_codes[key] = 0
+            for key, value in status_codes.items():
+                if value != 0:
+                    print(f"{key}: {value}")
+                    status_codes[key] = 0
 
-                total_size = 0
-    except (KeyboardInterrupt, EOFError):
-        print(f"Files size: {total_size}")
-
-        for key, value in status_codes.items():
-            if value != 0:
-                print(f"{key}: {value}")
-                status_codes[key] = 0
-
-        total_size = 0
+            total_size = 0
