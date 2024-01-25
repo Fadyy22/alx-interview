e34195aa31d6adc006226a2da60613db3bea1f5f
Fadyy22 <fadyalaa441@gmail.com>
2024-01-26 00:58:07 +0200

    parse the log from stdin

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
 """script that reads stdin line by line and computes metrics"""
+if __name__ == "__main__":
+    counter = 0
+    total_size = 0
+    status_codes = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+    }
 
-counter = 0
-total_size = 0
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-}
+    while True:
+        counter += 1
+        log = input().split()
+        file_size = int(log[-1])
+        status_code = log[-2]
 
-while True:
-    counter += 1
-    log = input().split()
-    file_size = int(log[-1])
-    status_code = log[-2]
+        try:
+            status_codes[status_code] += 1
+        except Exception:
+            pass
 
-    try:
-        status_codes[status_code] += 1
-    except Exception:
-        pass
+        total_size += file_size
 
-    total_size += file_size
+        if counter % 10 == 0:
+            print(f"Files size: {total_size}", flush=True)
 
-    if counter % 10 == 0:
-        print(f"Files size: {total_size}")
+            for key, value in status_codes.items():
+                if value != 0:
+                    print(f"{key}: {value}", flush=True)
+                    status_codes[key] = 0
 
-        for key, value in status_codes.items():
-            if value != 0:
-                print(f"{key}: {value}")
-                status_codes[key] = 0
-
-        total_size = 0
+            total_size = 0
