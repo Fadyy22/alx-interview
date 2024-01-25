f4610661c953225c3e3773f81b5b97b983c05551
Fadyy22 <fadyalaa441@gmail.com>
2024-01-26 00:45:19 +0200

    parse the log from stdin

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+counter = 0
+total_size = 0
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+}
+
+while True:
+    counter += 1
+    log = input().split()
+    file_size = int(log[-1])
+    status_code = log[-2]
+
+    try:
+        status_codes[status_code] += 1
+    except Exception:
+        pass
+
+    total_size += file_size
+
+    if counter % 10 == 0:
+        print(f"Files size: {total_size}")
+
+        for key, value in status_codes.items():
+            if value != 0:
+                print(f"{key}: {value}")
+                status_codes[key] = 0
+
+        total_size = 0
